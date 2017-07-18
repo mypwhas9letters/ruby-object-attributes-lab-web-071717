12343acd550b711f19ba84f365a14f9d8bbfd22f
Hui Wang <huiw3939@gmail.com>
2017-07-18 18:57:01 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+def name=(newname)
+  @name = newname
+end
+
+def name
+  @name
+end
+
+def breed=(newbreed)
+  @breed = newbreed
+end
+
+def breed
+  @breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+def name=(newname)
+  @name = newname
+end
+
+def name
+  @name
+end
+
+def job=(newjob)
+  @job = newjob
+end
+
+def job
+  @job
+end
+
+
+end
